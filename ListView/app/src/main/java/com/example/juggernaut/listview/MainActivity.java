d4bc8df8821f34e301373e8a3e40970f64605157
ashu94 <singhjug1994@gmail.com>
2016-02-10 17:33:38 +0530

    ListView example added

---
+++ ListView/app/src/main/java/com/example/juggernaut/listview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.juggernaut.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    String [] strings = {"mon" , "tue" , "wed"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, "position"+position, Toast.LENGTH_SHORT).show();
+    }
+}
