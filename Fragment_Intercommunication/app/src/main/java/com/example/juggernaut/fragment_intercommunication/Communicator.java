841186107e60ba5addce327ecc9f0bf31c273fab
ashu94 <singhjug1994@gmail.com>
2016-02-14 22:47:42 +0530

    structuring the project

--- Fragment_Intercommunication/app/src/main/java/com/example/juggernaut/fragment_intercommunication/Communicator.java
+++
@@ -1,8 +0,0 @@
-package com.example.juggernaut.fragment_intercommunication;
-
-/**
- * Created by juggernaut on 3/2/16.
- */
-public interface Communicator {
-    public void respond (String data);
-}
--- Fragment_user_interface/app/src/main/java/com/example/juggernaut/fragment_user_interface/Communicator.java
+++
@@ -1,8 +0,0 @@
-package com.example.juggernaut.fragment_user_interface;
-
-/**
- * Created by juggernaut on 3/2/16.
- */
-public interface Communicator {
-    public void respond (int i);
-}
