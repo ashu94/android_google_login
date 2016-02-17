40ebc77f965ebb6b2aede6f218dec8c743fc001e
ashu94 <singhjug1994@gmail.com>
2016-02-17 14:13:13 +0530

    button example added

---
+++ Button_click_1/app/src/main/java/com/example/juggernaut/button_click_1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.juggernaut.button_click_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("juggernaut", "button was click");
+
+    }
+}
