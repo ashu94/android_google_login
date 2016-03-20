82ec45113f9147acf173f5e4871de145e48e524d
ashu94 <singhjug1994@gmail.com>
2016-03-20 20:37:04 +0530

    sandbox example initated

---
+++ share_data_between_app/app1/app/src/main/java/com/example/juggernaut/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.juggernaut.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.et_value);
+    }
+
+    public void save(View view){
+
+    }
+}
