416bac739005e8325cad3aa3750ad7d56b4bef1c
ashu94 <singhjug1994@gmail.com>
2016-03-22 23:48:10 +0530

    sandbox example added

--- share_data_between_app/App2/app/src/main/java/com/example/juggernaut/app2/MainActivity.java
+++ share_data_between_app/App2/app/src/main/java/com/example/juggernaut/app2/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.juggernaut.app2;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    EditText et;
+    String packageName = "com.example.juggernaut.myapplication";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        et = (EditText) findViewById(R.id.editText);
+    }
+
+    public void load (View view){
+
+        PackageManager packageManager = getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+            String fullPath =applicationInfo.dataDir + "/files/juggernaut.txt";
+            readFile(fullPath);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv.setTextColor(Color.RED);
+            tv.setText(e + "");
+        }
+
+    }
+
+    public void readFile (String filePath){
+        FileInputStream fileInputStream = null;
+        try {
+           fileInputStream = new FileInputStream(new File(filePath));
+          int read = -1;
+          StringBuffer stringBuffer = new StringBuffer();
+
+            while ((read = fileInputStream.read()) != -1){
+                stringBuffer.append((char)read);
+            }
+
+            et.setText(stringBuffer);
+            tv.setTextColor(Color.GREEN);
+            tv.setText(stringBuffer+ " was read successfully from "+filePath);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  finally {
+            if (fileInputStream != null){
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- share_data_between_app/app1/app/src/main/java/com/example/juggernaut/myapplication/MainActivity.java
+++ share_data_between_app/app1/app/src/main/java/com/example/juggernaut/myapplication/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.juggernaut.myapplication;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText editText;
+    TextView status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
+        status = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.et_value);
     }
 
     public void save(View view){
 
+
+        File file = null;
+        String text1 = editText.getText().toString();
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("juggernaut.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write(text1.getBytes());
+            status.setTextColor(Color.GREEN);
+            status.setText(text1+" written to "+file.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
